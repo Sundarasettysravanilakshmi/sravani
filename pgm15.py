92a42e4b445a3ea5dcc7de09f92c4440004c056c
Sundarasettysravanilakshmi <41770931+Sundarasettysravanilakshmi@users.noreply.github.com>
2018-08-12 04:06:55 -0700

    Create pgm15.py

---
+++ pgm15.py
@@ -0,0 +1,10 @@
+s,p=input().split()
+s,p=int(s),int(p)
+k=""
+for i in range(s+1,p):
+    if(i%2==0):
+        if i<2:
+            k=" "
+        else:k=" "
+        print(i,end=k)
+                
