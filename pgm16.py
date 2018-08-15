077113a8fc450ad9093f2d3270d118f31744dd85
Sundarasettysravanilakshmi <41770931+Sundarasettysravanilakshmi@users.noreply.github.com>
2018-08-15 03:54:20 -0700

    Create pgm16.py

---
+++ pgm16.py
@@ -0,0 +1,8 @@
+s,p=map(int,raw_input().split())
+for i in range(s+1,p):
+        if(i>1):
+                for k in range(2,i):
+                        if(i%k==0):
+                               break
+                else:
+                       print i
