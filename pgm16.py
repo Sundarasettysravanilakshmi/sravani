2af5aeb20e5bbf79dda2ae43d7e1fea38e29a0ba
Sundarasettysravanilakshmi <41770931+Sundarasettysravanilakshmi@users.noreply.github.com>
2018-08-15 04:06:35 -0700

    Update pgm16.py

--- pgm16.py
+++ pgm16.py
@@ -1,8 +1,8 @@
 s,p=map(int,raw_input().split())
 for i in range(s+1,p):
-        if(i>1):
+          if(i>1):
                 for k in range(2,i):
-                        if(i%k==0):
+                             if(i%k== 0):
                                break
                 else:
                        print i
